4b88d46f3d1093cde8c118213001a74e9b7f7f18
Christian Alares <christian.alares@gmail.com>
2019-01-25 12:53:30 +0100

    socket works

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log( 'main.js' )
+$(document).ready(function () {
+	// start up the SocketIO connection to the server - the namespace 'test' is also included here if necessary
+	const socket = io.connect('http://172.16.20.235:5000')
+
+	// this is a callback that triggers when the "my response" event is emitted by the server.
+	socket.on('space', () => {
+		
+	})
+})
