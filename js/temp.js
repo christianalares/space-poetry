220ef7006b9ea871cf52d35564fc1a99ab882486
Mattias Söderström <mattias.ep@gmail.com>
2019-01-25 12:53:56 +0100

    ..

---
+++ js/temp.js
@@ -0,0 +1,14 @@
+const poetryContainer = document.getElementById('poetry')
+
+const poetryArr = [
+	`I rymden finns det stjärnor 
+	som liknar glänsande pärlor. 
+	Det är spännande att titta upp
+	i  rymden och se tecken 
+	tillexempel karlavagnarna.
+	Det ser overkligt ut.`
+]
+
+const writePoetry = (poetry) => {
+
+}
